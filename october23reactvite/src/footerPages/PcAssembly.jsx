212c8474df7709a405a1f58f5a30eb646c4a2792
joeyboi <66668752+joeyyvel@users.noreply.github.com>
2024-03-04 00:25:46 +0800

    update 12:26am march 4 2024

--- october23reactvite/src/footerPages/PcAssembly.jsx
+++ october23reactvite/src/footerPages/PcAssembly.jsx
@@ -12,6 +12,48 @@ const PcAssembly = () => {
         <div>
           <img src={pcPicture} alt="PC picture" />
         </div>
+        <div>
+          <p>
+            There are plenty of pre-built gaming PCs on the market, but there’s
+            nothing quite like the satisfaction of using one that you assembled
+            yourself. While the process of assembling a computer isn’t
+            difficult, it’s daunting the first time you do it. For those
+            embarking on their first build – or perhaps the first build in
+            several years – we’ve put together a step-by-step guide to building
+            a PC.{" "}
+          </p>
+          <p>
+            Before we start, know that this is a guide exclusively dedicated to
+            assembly. That means you’ll need to pick out your parts first. Our
+            list of tested and approved cases, CPUs, GPUs, motherboards, SSDs,
+            power supplies and RAM can help you choose key components. Or you
+            could check out our Best PC Builds page if you`re looking to
+            assemble a system at or under a specific price.
+          </p>
+          <p>
+            The other thing to keep in mind is that no two builds are identical.
+            The order we’re going in here is based partly on preference and on
+            the needs of our specific build. For instance, if you have a large
+            aftermarket cooler that overhangs the RAM slots, you may need to
+            install your memory first. Or if you`re installing an AIO cooler
+            radiator at the top of your case, you`ll often need to plug in any
+            power or fan cables up there before the cooler blocks access to the
+            ports and headers.
+          </p>
+          <p>
+            Likewise, if you have a particularly small or cramped case and/or a
+            huge high-end graphics card, you may need to install things in a
+            slightly different order, since the graphics card will block access
+            to many ports, slots and connectors.
+          </p>
+          <p>
+            In short, don`t be surprised if you need to backtrack and pull out a
+            part here or there to accommodate the quirks of your particular
+            build. But don`t get frustrated, as complications like this are
+            common, even for people who have been regularly building PCs for
+            decades.
+          </p>
+        </div>
       </div>
     </>
   );
